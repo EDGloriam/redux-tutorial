852c543a344cffa28d7da3386fb88482f6f09869
Александр Гнатюк <a.gnatiuk@greenwood.com.ua>
2018-10-29 18:04:23 +0200

    facabook auth

--- new/my-app/src/components/User/User.js
+++ new/my-app/src/components/User/User.js
@@ -1,13 +1,46 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import FacebookLogin from 'react-facebook-login'
 
 export default class User extends Component {
+    state = {
+        isLoggedIn: false,
+        userId: '',
+        name: '',
+        picture: ''
+    }
+    responseFacebook = response => {
+        this.setState({
+            isLoggedIn: true,
+            userId: response.userID,
+            name: response.name,
+            picture: response.picture.data.url
+        })
+    }
+    componentClicked = () => console.log('cklicked');
+
     render(){
-        const { name } = this.props
+        let fbContent;
+        // const { name } = this.props
+
+        if (this.state.isLoggedIn){
+            fbContent = (<div>
+                <img src={this.state.picture} alt='Avatar'/>
+                <p>Привет,<br /> {this.state.name}!</p>
+                </div>            
+            );
+        } else {
+            fbContent = <FacebookLogin
+            appId="2335375206479800"
+            autoLoad={true}
+            fields="name,email,picture"
+            onClick={this.componentClicked}
+            callback={this.responseFacebook} />
+        }
 
         return (
         <div className="ib user">
-            <p>Привет, {name}!</p>
+            {fbContent}
         </div>
         )
     }
--- new/my-app/src/store/configStore.js
+++ new/my-app/src/store/configStore.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from './reducers/indexReducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+// import {ping } from './enhancers/ping'
 
 export const myStore = createStore(rootReducer, applyMiddleware(thunk, logger));
 
